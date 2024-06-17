65a6e856cdec230f666444b2aef954f5c3a7b64d
kermattC <matthew.chan5@uoit.net>
2024-06-17 18:50:58 -0400

    Added styling for project card, wip for thumbnai l image

--- portfolio/src/components/projectCard.tsx
+++ portfolio/src/components/projectCard.tsx
@@ -1,16 +1,44 @@
+import './../styles/projectCard.css';
+
+import { useState } from 'react';
+
 interface ProjectCardProps {
     title: string,
     description: string,
+    timeline: string,
     thumbnail: string
 }
 
-const ProjectCard = ( { title, description, thumbnail}: ProjectCardProps ) => {
+const ProjectCard = ( { title, description, timeline, thumbnail}: ProjectCardProps ) => {
+    
+    const [isHovered, setIsHovered] = useState<boolean>(false);
+    
+    function cardEnter(event: React.MouseEvent<HTMLDivElement>) {
+    // function cardEnter(event: any) {
+        setIsHovered(true);
+    }
+
+    function cardLeave(event: React.MouseEvent<HTMLDivElement>) {
+        console.log("Leaving: ", event)
+        setIsHovered(false);
+    }
+
     return (
-        <>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <p>Temporary: {thumbnail}</p>
-        </>
+        <div className='projectContainer'>
+            <div className={isHovered ? 'projectCard-hovered' : 'projectCard'} 
+                onMouseEnter={(e) => cardEnter(e)} 
+                onMouseLeave={(e) => cardLeave(e)}
+            >
+                <div className='left-details'>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                </div>
+                <div className='right-details'>
+                    <p>{timeline}</p>
+                    <img src="/figures/msc-thumbnail.png" alt="Thumbnail"/>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- portfolio/src/components/projects.tsx
+++ portfolio/src/components/projects.tsx
@@ -5,23 +5,25 @@ import projectData from './../daterz/projects.json';
 interface Project {
     title: string,
     description: string,
+    timeline: string,
     thumbnail: string
 }
 
 const projects: Project[] = projectData.projects;
 
 export default function Projects() {
-    console.log("Project data: ", projects);
+    // console.log("Project data: ", projects);
     
     return (
         <div>
-            <h3>Projects section</h3>
+            <h3>Placeholder title - Projects section</h3>
             {                
                 projects.map((project, index) => {
                     return(
                         <ProjectCard
                             key={index}
-                            title={project.title} 
+                            title={project.title}
+                            timeline={project.timeline}
                             description={project.description} 
                             thumbnail={project.thumbnail}
                         />
