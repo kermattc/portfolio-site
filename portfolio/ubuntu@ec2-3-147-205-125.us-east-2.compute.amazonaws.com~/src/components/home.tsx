5a2ed580c9deed1dbf93c8c005999aa8e13411f0
kermattC <matthew.chan5@uoit.net>
2024-08-23 22:49:57 -0400

    Fixed typo in new openNewTab function preventing new page from being opened

--- portfolio/ubuntu@ec2-3-147-205-125.us-east-2.compute.amazonaws.com~/src/components/home.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-    return (
-        <>  
-            <h3>Home section</h3>
-            <p>This is where the home stuff is. Maybe I might take this part out</p>
-        </>
-    )
-}
