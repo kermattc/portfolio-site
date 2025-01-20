0343a4de5f4dab73d0c8cf0887b9f4b988c8f309
kermattC <chan.matt624@gmail.com>
2025-01-20 18:07:01 -0500

    Updated UX for project cards, disabled portfolio card because it was redundant and placed a notice, changed baseball stats to modal that plays demo gif because the site is broken

--- portfolio/src/components/modal.tsx
+++ portfolio/src/components/modal.tsx
@@ -1,4 +1,5 @@
 import './../styles/modal.css';
+import baseballGif from '../assets/gifs/baseball-stats-demo.gif';
 import { AiOutlineClose } from "react-icons/ai";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,10 +8,11 @@ import { useRef, useEffect } from 'react';
 interface ModalProps {
     modalToggle: (isModalOpen: boolean) => void,
     openLink: () => void,
-    demoLink: string
+    demoLink: string,
+    type: string
 }
 
-const Modal = ({ modalToggle, openLink, demoLink }: ModalProps) => {
+const Modal = ({ modalToggle, openLink, demoLink, type }: ModalProps) => {
     const modalRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
@@ -24,39 +26,68 @@ const Modal = ({ modalToggle, openLink, demoLink }: ModalProps) => {
             }
         }
 
+        const handleKeyDown = (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                modalToggle(false);
+            }
+        };
+
         // bind event listener to the document when the component is mounted
         document.addEventListener("mousedown", handleClickOutside);
+        document.addEventListener("keydown", handleKeyDown);
 
         // cleanup function. called when the dependency changes, or in this case when I want to unmount this modal component. Which in this case will always change to false
         return () => {
             // unbind event listener on clean up, prevent memory leaks and all that
             document.removeEventListener("mousedown", handleClickOutside)
-        }
+            document.removeEventListener("keydown", handleKeyDown)
 
-    }, [modalToggle])
+        };
+    }, [modalToggle]);
 
     return (
-        <div className='modalBackground'>
-            <div className='modalContainer' ref={modalRef}>
-                <div className='exitBtn'>
-                    <AiOutlineClose className='exitIcon' onClick={() => modalToggle(false)}
-                        />
-                </div>
-                <div className='titleContainer'>
-                    <h1 className="modalTitle">Demo Video</h1>
-                </div>
-                <div className='body'>
-                    <div className="ratio ratio-16x9">
-                        <iframe src={demoLink} title="YouTube video" allowFullScreen></iframe>
-                    </div>    
-                </div>
-                <div className="footer">
-                    <button className='cancelBtn' onClick={() => modalToggle(false)}>Cancel</button>
-                    <button className='goBtn' onClick={openLink}>Go to thesis page</button>
+        <>  
+            <div className='modalBackground'>
+                <div className='modalContainer' ref={modalRef}>
+                    <div className='exitBtn'>
+                        <AiOutlineClose className='exitIcon' onClick={() => modalToggle(false)}
+                            />
+                    </div>
+                    <div className='titleContainer'>
+                    { type === 'thesis' && (
+                        <h1 className="modalTitle">Demo Video</h1>
+                    )}
+                    { type === 'baseball-maintenance' && (
+                        <h1 className="modalTitle">Baseball Stats Website Demo</h1>
+                    )}
+                    </div>
+                    <div className='body'>
+                    { type === 'thesis' && (
+                        <div className="ratio ratio-16x9">
+                            <iframe src={demoLink} title="YouTube video" allowFullScreen></iframe>
+                        </div>    
+                    )}
+                    { type === 'baseball-maintenance' && (
+                        <div className="ratio ratio-16x9">
+                            <img src={baseballGif} alt="loading..." title="Baseball Stats Website Demo"></img>
+                        </div>    
+                    )}
+                    </div>
+                    { type === 'thesis' && (
+                        <div className="footer">
+                            <button className='cancelBtn' onClick={() => modalToggle(false)}>Cancel</button>
+                            <button className='goBtn' onClick={openLink}>Go to thesis page</button>
+                        </div>
+                    )}
+                    { type === 'baseball-maintenance' && (
+                        <div className="footer">
+                            <button className='cancelBtn' onClick={() => modalToggle(false)}>Cancel</button>
+                            <div className='maintenance-notice'> Site is currently down, sorry!</div>
+                        </div>
+                    )}
                 </div>
             </div>
-        </div>
-
+        </>
     )
 }
 
--- portfolio/src/components/projectCard.tsx
+++ portfolio/src/components/projectCard.tsx
@@ -22,29 +22,46 @@ const images: { [key:string]: string} = {
     'baseballThumbnail': require('./../figures/baseball-stats-thumbnail.png'),
     'guitarLookupThumbnail': require('./../figures/guitar-lookup-thumbnail.png'),
     'portfolioThumbnail': require('./../figures/portfolio-thumbnail.png'),
-
-
 }
 
 const ProjectCard = ( { title, description, timeline, thumbnail, link, demoLink, skills, type }: ProjectCardProps ) => {
 
     const [isHovered, setIsHovered] = useState<boolean>(false);
+    const [isPortfolioHovered, setIsPortfolioHovered] = useState<boolean>(false);
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [isNoticeVisible, setIsNoticeVisible] = useState<boolean>(false);
 
     const pcThumbnail = images[thumbnail]
+    let timer: NodeJS.Timeout;
 
     function cardEnter(event: React.MouseEvent<HTMLDivElement>) {
-        setIsHovered(true);
+        if (type === 'project' || type === 'thesis' || type === 'baseball-maintenance') { 
+            setIsHovered(true);
+        } else if (type === 'portfolio') {
+            setIsPortfolioHovered(true);
+            timer = setTimeout(() => setIsNoticeVisible(true), 750); // show notice after hovering this div for a bit
+        }
     }
 
     function cardLeave(event: React.MouseEvent<HTMLDivElement>) {
-        console.log("Leaving: ", event)
-        setIsHovered(false);
+        if (type === 'project' || type === 'thesis' || type === 'baseball-maintenance') { 
+            setIsHovered(false);
+        } else if (type === 'portfolio') {
+            setIsPortfolioHovered(false);
+            clearTimeout(timer);
+        }
+        
+        // bug will occur where hovering over other portfolio cards will also reveal the notice
+        if (type === 'portfolio') {
+            setIsNoticeVisible(false); 
+        }
     }
 
     function openModal(): void {
-        if (type === 'thesis') {
+        if (type === 'thesis' || type === 'baseball-maintenance') {
             setIsModalOpen(true);
+        } else if (type === 'portfolio') { 
+            return
         } else {
             openInNewTab()
         }
@@ -57,7 +74,10 @@ const ProjectCard = ( { title, description, timeline, thumbnail, link, demoLink,
     return (        
         <>
             <div className='projectContainer'  onClick={openModal}>
-                <div className={isHovered ? 'projectCard-hovered' : 'projectCard'} 
+                <div className={`
+                    ${isHovered ? 'projectCard-hovered' : 'projectCard'}
+                    ${isPortfolioHovered ? 'portfolioCard-hovered': 'portfolioCard'}
+                    `}
                     onMouseEnter={(e) => cardEnter(e)} 
                     onMouseLeave={(e) => cardLeave(e)}
                 >
@@ -74,10 +94,21 @@ const ProjectCard = ( { title, description, timeline, thumbnail, link, demoLink,
                     <div className='skillsContainer'>
                         <SkillsNugget skills={skills}/>
                     </div>
+                    { title === 'Baseball Stats Website' && (
+                        <div className='maintenance-notice'>
+                            Under maintenance
+                        </div>
+                        )
+                    }
+                    { isNoticeVisible && type === 'portfolio' && (
+                        <div className="notice">
+                            You're already here!
+                        </div>
+                    )}
                 </div>
             </div>
             { isModalOpen ? 
-                <Modal modalToggle={setIsModalOpen} openLink={openInNewTab} demoLink={demoLink}/>
+                <Modal modalToggle={setIsModalOpen} openLink={openInNewTab} demoLink={demoLink} type={type}/>
                 : 
                 null
             }
